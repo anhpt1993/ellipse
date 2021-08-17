6018a99d814c404f518c8aebcd28b71ff188ff9a
anhpt1993 <88441530+anhpt1993@users.noreply.github.com>
2021-08-17 22:01:28 +0700

    2201_170821 revise ellipse.py file

--- ellipse.py
+++ ellipse.py
@@ -1,4 +1,5 @@
 import turtle as t
+import random
 
 def ellipse(r):
     for i in range(2):
@@ -7,18 +8,16 @@ def ellipse(r):
 
 count = 0
 t.speed(100)
-
+colors = ["blue", "orange", "green", "red", "purple", "brown", "pink", "gray", "olive", "cyan"]
 while True:
     t.right(45+count*5)
-    t.penup()
-    t.goto(count*5, count*-5)
     t.pendown()
+    t.pencolor(random.choice(colors))
     ellipse(200)
     count += 1
     if count > 72:
         break
     t.home()
-
 
 t.hideturtle()
 t.done()
